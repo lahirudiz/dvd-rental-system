eb9b631268f95647563590bd9146422e6d9292ba
Lahiru Dssanyaka <lahirudiz@users.noreply.github.com>
2016-07-28 20:32:06 +0530

    Update help.php

--- help.php
+++ help.php
@@ -13,7 +13,7 @@ include('nav.php');
 <body>
 
 <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-<h1 class="page-header">Help</h1>
+<h1 class="page-header">Help </h1>
 
 </div>
 
